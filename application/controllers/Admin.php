777c6aeb3b833de74095b1fef4067c687458a092
Denisbar <bar_deniska@mail.ru>
2016-08-29 13:47:47 +0300

    Add update functions

--- application/controllers/Admin.php
+++ application/controllers/Admin.php
@@ -12,43 +12,92 @@ class Admin extends CI_Controller
     {
         parent::__construct();
         $this->load->model('price_model');
+        $this->load->model('admin_model');
         $this->load->helper('url');
     }
+//
+//    public function signup()
+//    {
+//        $this->load->view('admin/signup_form');
+//    }
+//
+//    public function create_member()
+//    {
+//
+//        $this->load->library('form_validation');
+//        $this->form_validation->set_rules('first_name', 'Name', 'trim|reqired');
+//        $this->form_validation->set_rules('last_name', 'Last Name', 'trim|reqired');
+//        $this->form_validation->set_rules('email_address', 'Email Address', 'trim|reqired|valid_email');
+//
+//        $this->form_validation->set_rules('username', 'Username', 'trim|reqired|min_lenght[4]');
+//        $this->form_validation->set_rules('password', 'Password', 'trim|reqired|min_lenght[4]');
+//        $this->form_validation->set_rules('password2', 'Password Conformation', 'trim|reqired|matches[password]');
+//
+//        if ($this->form_validation->run() == FALSE) {
+//
+//            $this->signup();
+//        } else {
+//            $this->load->model('membership_model');
+//            if ($query = $this->membership_model->create_member()) {
+//
+//                $this->signup_successful();  //Успешная регистрация
+//            } else {
+//                $this->load->view('admin/signup_form');
+//            }
+//        }
+//    }
+//
+//    public function insert_text_area()
+//    {
+//        $this->load->model('price_model');
+//        $this->price_model->insert_area();
+//    }
 
-    public function signup()
+    public function contact_view()
     {
-        $this->load->view('admin/signup_form');
+        $this->load->view('admin/edit_contact');
+    }
+// Update contact form
+    public function update_country()
+    {
+        $this->load->model('admin_model');
+        $this->admin_model->update_country();
+        $this->load->view('admin/adminTamplate/header');
     }
 
-    public function create_member()
+    public function update_city()
     {
-
-        $this->load->library('form_validation');
-        $this->form_validation->set_rules('first_name', 'Name', 'trim|reqired');
-        $this->form_validation->set_rules('last_name', 'Last Name', 'trim|reqired');
-        $this->form_validation->set_rules('email_address', 'Email Address', 'trim|reqired|valid_email');
-
-        $this->form_validation->set_rules('username', 'Username', 'trim|reqired|min_lenght[4]');
-        $this->form_validation->set_rules('password', 'Password', 'trim|reqired|min_lenght[4]');
-        $this->form_validation->set_rules('password2', 'Password Conformation', 'trim|reqired|matches[password]');
-
-        if ($this->form_validation->run() == FALSE) {
-
-            $this->signup();
-        } else {
-            $this->load->model('membership_model');
-            if ($query = $this->membership_model->create_member()) {
-
-                $this->signup_successful();  //Успешная регистрация
-            } else {
-                $this->load->view('admin/signup_form');
-            }
-        }
+        $this->load->model('admin_model');
+        $this->admin_model->update_city();
     }
 
-    public function insert_text_area()
+    public function update_adress()
     {
-        $this->load->model('price_model');
-        $this->price_model->insert_area();
+        $this->load->model('admin_model');
+        $this->admin_model->update_adress();
+    }
+
+    public function update_tel()
+    {
+        $this->load->model('admin_model');
+        $this->admin_model->update_tel();
+    }
+
+    public function update_name()
+    {
+        $this->load->model('admin_model');
+        $this->admin_model->update_name();
+    }
+
+    public function update_mob()
+    {
+        $this->load->model('admin_model');
+        $this->admin_model->update_mob();
+    }
+
+    public function update_email()
+    {
+        $this->load->model('admin_model');
+        $this->admin_model->update_email();
     }
 }
